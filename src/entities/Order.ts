4e0f52c4d16993ae41eec2a36dd9859814434d8c
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-03-26 15:10:41 -0300

    fix user table, user_id field

--- src/entities/Order.ts
+++ src/entities/Order.ts
@@ -16,6 +16,7 @@ export class Order {
     status: boolean;
 
     @ManyToOne(() => User, (user) => user.order)
+    @JoinColumn({ name: 'user_id' })
     user: User;
 
     @ManyToOne(() => Customer, (customer) => customer.order)
